fd3d485a7c3f824e080fe0879dc8131ea7da7f8c
HP Laptop <katharina.slava@gmail.com>
2023-11-26 14:51:50 -0500

    employee-register-project

---
+++ src/main/java/com/cydeo/empregproject/EmpRegProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.empregproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpRegProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpRegProjectApplication.class, args);
+    }
+
+}
